5c95d396f7cf698fa8f5ab253f1d886f56acb731
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-18 00:25:34 +0300

    refactor: update Temperature Pipe to get two decimal points as output

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -6,10 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
   transform(
-    value: string | number,
+    value: string | number | null,
     inputType: 'cel' | 'fah',
     outputType?: 'cel' | 'fah'
   ) {
+    if (!value) {
+      return value;
+    }
+
     let val: number;
 
     if (typeof value === 'string') {
@@ -36,6 +40,6 @@ export class TemperaturePipe implements PipeTransform {
       symbol = outputType === 'cel' ? '°C' : '°F';
     }
 
-    return `${outputTemp} ${symbol}`;
+    return `${outputTemp.toFixed(2)} ${symbol}`;
   }
 }
