1b1a92212be7d80db01b421496793224bd84a372
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-18 00:07:31 +0300

    refactor: make Temperature Pipe configurable

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -5,7 +5,37 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class TemperaturePipe implements PipeTransform {
-  transform(value: any, ...args: any[]) {
-    return value + ' - transformed';
+  transform(
+    value: string | number,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ) {
+    let val: number;
+
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: '°C' | '°F';
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? '°C' : '°F';
+    } else {
+      symbol = outputType === 'cel' ? '°C' : '°F';
+    }
+
+    return `${outputTemp} ${symbol}`;
   }
 }
